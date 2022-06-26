58a45206d93320b6096d71056101291381500d80
Cameron McGinley <mcginleycameron@gmail.com>
2022-06-25 21:28:49 -0400

    Move data load to main, minor improvements

--- src/hyperparam_finder.py
+++ src/hyperparam_finder.py
@@ -6,6 +6,9 @@ best params, along with the accuracy obtained, are printed back
 into algorithms.yml under 'param_results' for each algorithm
 """
 
+from numpy import ndarray
+from yaml import safe_load, safe_dump
+from pathlib import Path
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -14,12 +17,11 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression  # noqa, expected unused
 from sklearn.svm import SVC  # noqa
 from sklearn.ensemble import RandomForestClassifier  # noqa
-import yaml
-
-from pathlib import Path
 
 
-def find_hyperparams(algorithmsyml_path: str) -> None:
+def find_hyperparams(
+    algorithmsyml_path: str, X_train: ndarray, y_train: ndarray
+) -> None:
     """Run GridSeachCV on models stored in algorithms/algorithms.yml.
 
     This func reads in the data of models/algorithms from
@@ -29,22 +31,13 @@ def find_hyperparams(algorithmsyml_path: str) -> None:
 
     Args:
         algorithmsyml_path: File path to algorithms.yml
+        X_train: ndarray of dataset's X values
+        y_train: ndarray of dataset's y values
 
     """
-    iris = datasets.load_iris()
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        iris["data"], iris["target"], test_size=0.3, random_state=42
-    )
-
-    # Use GridSearchCV to find optimal hyperparameters for the models
-
-    # For the three classifiers we use LogisticRegression, SVC, and
-    # RandomForestClassifier
-
     # Read in our models and param_grids defined in the yaml
     with open(algorithmsyml_path, "r") as file:
-        data = yaml.safe_load(file)
+        data = safe_load(file)
 
     # For each model in the yaml, grid search against it's param
     # grid and output the results back into the yaml under 'param_results'
@@ -64,8 +57,8 @@ def find_hyperparams(algorithmsyml_path: str) -> None:
         # not been seen before future testing
         results = clf.fit(X_train, y_train)
 
-        print("Best Score: %s" % results.best_score_)
-        print("Best Hyperparameters: %s" % results.best_params_)
+        print(f"Best Score: {results.best_score_}")
+        print(f"Best Hyperparameters: {results.best_params_}")
 
         # Add results to the dictionary, then output to yaml
         data["algorithms"][model_func]["param_results"] = {
@@ -73,14 +66,25 @@ def find_hyperparams(algorithmsyml_path: str) -> None:
             "params": results.best_params_,
         }
         with open(algorithmsyml_path, "w") as file:
-            yaml.dump(data, file)
+            safe_dump(data, file)
 
 
-if __name__ == "__main__":
-    # Get algorithms.yml
+def main() -> None:
+    """Handle getting required info for and executing find_hyperparams()."""
+    # Get algorithms.yml path
     base_path = Path(__file__).parent
     algorithmsyml_path = (base_path / "../algorithms/algorithms.yml").resolve()
 
+    # Get dataset
+    iris = datasets.load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(
+        iris["data"], iris["target"], test_size=0.3, random_state=42
+    )
+
     # Find hyperparams for every model in algorithms.yml
     # Output results back to same yaml
-    find_hyperparams(algorithmsyml_path)
+    find_hyperparams(algorithmsyml_path, X_train, y_train)
+
+
+if __name__ == "__main__":
+    main()
